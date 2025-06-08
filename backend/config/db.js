10cc1eaf062d78882c113242b15612be87ee63bd
Surbhi Tomar <bhoomikatomar0510@gmail.com>
2025-06-08 20:45:03 +0530

    First commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Bhoomika-Tomar:food-delivery@cluster0.gwdtf.mongodb.net/food-delivery-website').then(()=>console.log("DB CONNECTED"));
+    await mongoose.connect('mongodb+srv://Bhoomika-Tomar:mongodbatlas@cluster0.gwdtf.mongodb.net/food-delivery-website').then(()=>console.log("DB CONNECTED"));
 }
